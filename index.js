3b0230f940b7add3f366a7a00287442267377137
AchmadArifaturrohman <140537284+AchmadArifaturrohman@users.noreply.github.com>
2024-10-22 17:17:23 +0700

    Modify Swagger in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const envPath =
-  process.env.NODE_ENV === "develompent"
+  process.env.NODE_ENV === "development"
     ? ".env"
     : `.env.${process.env.NODE_ENV}`;
 require("dotenv").config({ path: envPath });
@@ -10,13 +10,16 @@ const routes = require("./src/routes");
 const { PORT = 3111 } = process.env;
 const app = express();
 const errorHandler = require("./src/middlewares/errorHandler");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./api/openapi.json");
+
 require("./src/helpers/errors");
 
 app.use(cors());
 app.use(express.json()); // Sebagai middleware untuk mengubah req.body menjadi JSON
 
 app.use("/public", express.static(path.join(__dirname, "public")));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", routes);
 
 app.use((req, res) => {
